58fb6dc23bb4a1b3608c2557f5a04f026a130f00
DevKotlin1985 <142638449+DevKotlin1985@users.noreply.github.com>
2023-09-04 21:52:50 -0300

    Adicionado a fun matricular, constructor aluno, entre outras melhoridas no código.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,53 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {BASICO, INTERMEDIARIO, AVANCADO}
 
-class Usuario
+class Aluno(val nome: String, val nivel: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+data class ConteudoEducacional(var nome: String, val duracao: Int = 120) {
+    init { 
+    	require(duracao > 0) { "A duracao dos cursos deverão serem sempre positiva "}
+    }
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+}
+
+class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional>) { 
+	val inscritos = mutableListOf<Aluno>()
+    
+    fun matricular(aluno: Aluno) {  
+    	inscritos.add(aluno)
+        println(" O Aluno(a) ${aluno.nome} matriculou se no curso de $nome")
+        println()
+    
+    }
+
+    fun listarInscritos() { 
+    	println("Inscritos na formação $nome:")
+        inscritos.forEach { println(it.nome) }
+        println()
+    
     }
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+fun main(){
+    
+    val aluno1 = Aluno("Jessica", Nivel.BASICO)
+    val aluno2 = Aluno("Mariana", Nivel.AVANCADO)
+    val aluno3 = Aluno("Marcos", Nivel.INTERMEDIARIO)
+    
+    val conteudo1 = ConteudoEducacional("Lógica de Programação", 120)
+    val conteudo2 = ConteudoEducacional("Dominando Multiplas Linguagens", 240)
+  
+    
+    val formacao1 = Formacao("Lógica de Programação", mutableListOf(conteudo1))
+    val formacao2 = Formacao("Dominando Multiplas Linguagens", mutableListOf(conteudo2))
+    
+    formacao1.matricular(aluno1)
+    formacao1.matricular(aluno2)
+    formacao1.listarInscritos()
+
+    formacao2.matricular(aluno2)
+    formacao2.matricular(aluno3)
+    formacao2.listarInscritos()  
+    
 }
